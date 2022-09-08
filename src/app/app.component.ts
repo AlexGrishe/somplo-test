372358e9a5379a2652b06e1f416fe700ade16caa
AlexGrishe <sashka.greha@gmail.com>
2022-09-08 18:46:54 +0300

    Improve the function with the ability to download code in html format

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -1,10 +1,115 @@
-import { Component } from '@angular/core';
+import {Component, ElementRef, ViewEncapsulation} from '@angular/core';
+import {Observable} from "rxjs";
+import {ImageService} from "./image.service";
 
 @Component({
   selector: 'app-root',
   templateUrl: './app.component.html',
-  styleUrls: ['./app.component.css']
+  styleUrls: ['./app.component.css'],
+  encapsulation: ViewEncapsulation.None
 })
+
 export class AppComponent {
-  title = 'untitled';
+  title: string = 'Odessa-test';
+  selectedFiles?: FileList;
+  currentFile?: File;
+  progress = 0;
+  message = '';
+  preview = '';
+  imageInfos?: Observable<any>;
+  animatedOne: any = false;
+  animatedTwo: any = false;
+  animatedThree: any = false;
+  width: any = 200;
+  height: any = 200;
+  bottom: any = 0;
+  right: any = 0;
+  locationX: any = 0;
+  locationY: any = 0;
+  htmlFile: any;
+
+  constructor(private imageService: ImageService, private elementRef: ElementRef) {
+  }
+
+  ngOnInit(): void {
+    this.imageInfos = this.imageService.getFiles();
+  }
+
+  showAllHtml() {
+    alert(this.elementRef.nativeElement
+      .querySelector('.main-page')
+      .outerHTML
+    )
+  }
+
+  showImgBlockHtml() {
+    alert(this.elementRef.nativeElement
+      .querySelector('.upload-image-block__add-photo')
+      .outerHTML
+    )
+  }
+
+  //Improve the function with the ability to download code in html format
+  // getHTML() {
+  //   this.htmlFile = this.elementRef.nativeElement
+  //     .querySelector('.upload-image-block__location-photo')
+  //     .outerHTML
+  // }
+
+  selectFile(event: any): void {
+    this.message = '';
+    this.preview = '';
+    this.progress = 0;
+    this.selectedFiles = event.target.files;
+    if (this.selectedFiles) {
+      const file: File | null = this.selectedFiles.item(0);
+      if (file) {
+        this.preview = '';
+        this.currentFile = file;
+        const reader = new FileReader();
+        reader.onload = (e: any) => {
+          console.log(e.target.result)
+          this.preview = e.target.result;
+        };
+        reader.readAsDataURL(this.currentFile);
+      }
+    }
+  }
+
+  clickAnimationOne() {
+    this.animatedOne = !this.animatedOne
+  }
+
+  clickAnimationTwo() {
+    this.animatedTwo = !this.animatedTwo
+  }
+
+  clickAnimationThree() {
+    this.animatedThree = !this.animatedThree
+  }
+
+  imageWidth() {
+    this.width = document.getElementById('y')
+    this.right = document.getElementById('y')
+    this.right = (this.right.value - 200) / 2
+    this.width = this.width.value
+  }
+
+  imageHeight() {
+    this.height = document.getElementById('x')
+    this.bottom = document.getElementById('x')
+    this.bottom = (this.bottom.value - 200) / 2
+    this.height = this.height.value
+  }
+
+  imageLocationX() {
+    this.locationX = document.getElementById('xLocation')
+    this.locationX = this.locationX.value
+  }
+
+  imageLocationY() {
+    this.locationY = document.getElementById('yLocation')
+    this.locationY = this.locationY.value
+  }
+
 }
--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -1,14 +1,25 @@
 import { NgModule } from '@angular/core';
 import { BrowserModule } from '@angular/platform-browser';
+import {HttpClientModule} from "@angular/common/http";
 
 import { AppComponent } from './app.component';
+import {FormsModule, ReactiveFormsModule} from "@angular/forms";
+import { BrowserAnimationsModule } from '@angular/platform-browser/animations';
+import {MatFormFieldModule} from "@angular/material/form-field";
+import {MatSelectModule} from "@angular/material/select";
 
 @NgModule({
   declarations: [
     AppComponent
   ],
   imports: [
-    BrowserModule
+    BrowserModule,
+    ReactiveFormsModule,
+    FormsModule,
+    HttpClientModule,
+    BrowserAnimationsModule,
+    MatFormFieldModule,
+    MatSelectModule,
   ],
   providers: [],
   bootstrap: [AppComponent]
